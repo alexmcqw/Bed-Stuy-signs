e466d4a984f65fc98dbe2d7b7248423531981c74
alexmcqw <75506922+alexmcqw@users.noreply.github.com>
2025-11-30 18:41:15 -0500

    Extend x-axis to full width of graph area

--- script.js
+++ script.js
@@ -727,8 +727,10 @@ async function initTimeline() {
             
             // Create the plot
             const plot = window.Plot.plot({
+                width: timelineContainer.offsetWidth || 1200, // Use full container width
                 marginLeft: 200, // Space for business names
                 marginTop: 60, // Space for year labels
+                marginRight: 0, // No right margin to use full width
                 height: Math.max(400, businesses.length * 20), // Dynamic height based on number of businesses
                 x: {
                     type: "time",
